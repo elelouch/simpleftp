975a63c6533d2c5845db0754c569bcac56e9f4f9
Elias Rojas <rojas.elias@outlook.com>
2024-06-20 15:53:03 -0300

    Added opensock_tcpsrv, replaced EPRT by EPSV Added two new functions, the eprt by epsv change was because I found that is espv is less problematic in firewall terms, and seems more appropiate.

--- src/srv/myftpsrv_skel.c
+++ src/srv/myftpsrv_skel.c
@@ -9,14 +9,12 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
-
 #include <netinet/in.h>
 
 #define BUFSIZE 512
 #define CMDSIZE 4
 #define PARSIZE 100
 #define BACKLOG_SIZE 10
-#define EPRT_DELIM "|"
 
 #define MSG_220 "220 srvFtp version 1.0\r\n"
 #define MSG_331 "331 Password required for %s\r\n"
@@ -24,22 +22,27 @@
 #define MSG_530 "530 Login incorrect\r\n"
 #define MSG_221 "221 Goodbye\r\n"
 #define MSG_550 "550 %s: no such file or directory\r\n"
+#define MSG_502 "502 Command not implemented\r\n"
 #define MSG_299 "299 File %s size %ld bytes\r\n"
 #define MSG_226 "226 Transfer complete\r\n"
-#define MSG_227 "227 Ok. Server entering active mode...\r\n"
+#define MSG_227 "227 Ok. Server entering passive mode...\r\n"
 #define MSG_215 "215 UNIX Type\r\n"
-#define MSG_211 "211 RETR <path>: retrieve a file\r\n\
-                 211 QUIT: stop connection\r\n"
+#define MSG_211 "211 - RETR <path>: retrieve a file\n\
+                 211 - QUIT: stop connection\r\n"
 
 
-/* Used when a client wants to enter in passive mode sending EPRT.
-   the "|" character is used as delimiter for RFC 2428 suggestion
-   return : returns the transfer channel*/
-int eprt(int sd, char* param);
+/* Opens a socket for a tcp server.
+ * If service is null, then the OS chooses the port.
+ * return: server socket descriptor
+ */
+int opensock_tcpsrv(char *address, char *service);
+
+int epsv(int sd);
 /* handles peer connection in the main loop
-    ssd: slave socket descriptor
-    msd: master socket descriptor
-    return */
+ * ssd: slave socket descriptor
+ *  msd: master socket descriptor
+ *  return 
+ */
 void handle_connection(int, int);
 /**
  * function: send answer to the client
@@ -48,7 +51,7 @@ void handle_connection(int, int);
  * ...: variable arguments for economics of formats
  * return: true if not problem arise or else
  * notes: the MSG_x have preformated for these use
-**/
+ **/
 int send_ans(int sd, char *message, ...);
 
 /**
@@ -63,6 +66,7 @@ int send_ans(int sd, char *message, ...);
  *             you can check if you receive first USER
  *             and then check if you receive PASS
  **/
+
 int recv_cmd(int sd, char *operation, char *param);
 /**
  * function: RETR operation
@@ -88,19 +92,16 @@ int authenticate(int sd);
  *  sd: socket descriptor
  **/
 void operate(int sd);
-
-
 /**
  * Run with
  *         ./mysrv <SERVER_PORT>
  **/
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // reserve sockets and variables space
-    int msd, ssd, addrstat, yes = 1;
-    struct addrinfo hints;
-    struct addrinfo *p, *rp;
     struct sockaddr_storage peer_addr; // container big enough to support both ipv4 and ipv6
     socklen_t peer_len = sizeof peer_addr;
+    int msd = 0, ssd = 0;
 
     // arguments checking
     if (argc != 2) {
@@ -108,56 +109,15 @@ int main (int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_STREAM; /* TCP connection */
-    hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
-    hints.ai_protocol = 0;          /* Any protocol */
-    hints.ai_canonname = NULL;
-    hints.ai_addr = NULL;
-    hints.ai_next = NULL;
+    msd = opensock_tcpsrv(NULL, "3490");
 
-    addrstat = getaddrinfo(NULL, argv[1], &hints, &p);
-    if(addrstat != 0) {
-        fprintf(stderr, "getaddrinfo string: %s\n", gai_strerror(addrstat));
-        exit(EXIT_FAILURE);
-    }
-
-    // bind the first socket found
-    for(rp = p; rp != NULL; rp = rp->ai_next) {
-        msd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        if(msd == -1) {
-            perror("socket");
-            continue;
-        }
-
-        if(setsockopt(msd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-            perror("setsockopt");
-            exit(EXIT_FAILURE);
-        }
-
-        if(bind(msd, rp->ai_addr, rp->ai_addrlen) == -1){
-            perror("bind");
-            close(msd);
-        }
-        
-        break; // socket binded, success
-    }
-
-    if(!rp) {
-        fprintf(stderr, "Server failed to bind\n");
-        exit(EXIT_FAILURE);
-    }
-
-    freeaddrinfo(p);
-
-    if(listen(msd, BACKLOG_SIZE) == -1) {
+    if (listen(msd, BACKLOG_SIZE) == -1) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    for(;;) {
-        ssd = accept(msd, (struct sockaddr*) &peer_addr, &peer_len);
+    for (;;) {
+        ssd = accept(msd, (struct sockaddr *)&peer_addr, &peer_len);
         handle_connection(ssd, msd);
     }
 
@@ -166,16 +126,17 @@ int main (int argc, char *argv[]) {
     return 0;
 }
 
-void handle_connection(int ssd, int msd) {
-    if(ssd == -1) {
+void handle_connection(int ssd, int msd)
+{
+    if (ssd == -1) {
         perror("socket");
         return;
     }
 
-    if(!fork()) {
+    if (!fork()) {
         close(msd);
         send_ans(ssd, MSG_220);
-        if(!authenticate(ssd)){
+        if (!authenticate(ssd)) {
             close(ssd);
             exit(EXIT_FAILURE);
         }
@@ -188,8 +149,8 @@ void handle_connection(int ssd, int msd) {
     close(ssd);
 }
 
-
-int send_ans(int sd, char *message, ...){
+int send_ans(int sd, char *message, ...)
+{
     char buffer[BUFSIZE];
 
     va_list args;
@@ -198,28 +159,27 @@ int send_ans(int sd, char *message, ...){
     vsprintf(buffer, message, args);
     va_end(args);
     // send answer preformated and check errors
-    if(send(sd, buffer, strlen(buffer), 0) == -1){
+    if (send(sd, buffer, strlen(buffer), 0) == -1) {
         perror("send");
         return 0;
     }
 
-
-
     return 1;
 }
 
-int recv_cmd(int sd, char *operation, char *param) {
+int recv_cmd(int sd, char *operation, char *param)
+{
     char buffer[BUFSIZE], *token;
     int recv_s;
 
     // receive the command in the buffer and check for errors
     recv_s = recv(sd, buffer, BUFSIZE, 0);
-    if(recv_s == -1) {
+    if (recv_s == -1) {
         fprintf(stderr, "Error while receiving host command\n");
         return 0;
     }
 
-    if(recv_s == 0){
+    if (recv_s == 0) {
         printf("Host performed an orderly shutdown\n");
         exit(EXIT_FAILURE);
     }
@@ -232,23 +192,25 @@ int recv_cmd(int sd, char *operation, char *param) {
     // extract command received in operation if not set \0
     // extract parameters of the operation in param if it needed
     token = strtok(buffer, " ");
-    if (token == NULL || strlen(token) < 4) {
+    if (token == NULL || strlen(token) < 4){
         warn("not valid ftp command");
         return 0;
     }
-    if (operation[0] == '\0') strcpy(operation, token);
-    if (strcmp(operation, token)) {
+    if (operation[0] == '\0')
+        strcpy(operation, token);
+    if (strcmp(operation, token)){
         warn("Abnormal client flow: did not send %s command", operation);
         return 0;
     }
 
     token = strtok(NULL, " ");
-    if (token != NULL) strcpy(param, token);
+    if (token != NULL)
+        strcpy(param, token);
     return 1;
-
 }
 
-void retr(int sd, char *file_path) {
+void retr(int sd, char *file_path)
+{
     FILE *file;
     int bread;
     long fsize;
@@ -256,7 +218,7 @@ void retr(int sd, char *file_path) {
 
     // check if file exists if not inform error to client
     file = fopen(file_path, "r");
-    if(!file) {
+    if (!file) {
         send_ans(sd, MSG_550); // TODO: suggest to use LIST command
         return;
     }
@@ -271,22 +233,21 @@ void retr(int sd, char *file_path) {
     sleep(1);
 
     // send the file
-    while((bread = fread(buffer, BUFSIZE, 1, file))){
+    while ((bread = fread(buffer, BUFSIZE, 1, file))) {
         send(sd, buffer, bread, 0);
     }
 
-    if(ferror(file))
+    if (ferror(file))
         send_ans(sd, "Error while reading %s", file_path);
     else
         send_ans(sd, MSG_226);
 
-
     // close the file
     fclose(file);
-
 }
 
-int check_credentials(char *user, char *pass) {
+int check_credentials(char *user, char *pass)
+{
     FILE *file;
     char *path = "./ftpusers", *line = NULL, credentials[100];
     size_t line_size = 0;
@@ -296,97 +257,149 @@ int check_credentials(char *user, char *pass) {
     sprintf(credentials, "%s:%s", user, pass);
 
     // check if ftpusers file it's present
-    if ((file = fopen(path, "r"))==NULL) {
+    if ((file = fopen(path, "r")) == NULL)
+    {
         warn("Error opening %s", path);
         return 0;
     }
-    while (getline(&line, &line_size, file) != -1) {
+    while (getline(&line, &line_size, file) != -1)
+    {
         strtok(line, "\n");
-        if (strcmp(line, credentials) == 0) {
+        if (strcmp(line, credentials) == 0)
+        {
             found = 1;
             break;
         }
     }
     fclose(file);
-    if (line) free(line);
+    if (line)
+        free(line);
     return found;
 }
 
-int authenticate(int sd) {
+int authenticate(int sd)
+{
     char user[PARSIZE], pass[PARSIZE];
 
-    if(!recv_cmd(sd, "USER", user)){
+    if (!recv_cmd(sd, "USER", user))
+    {
         return 0;
     }
 
     send_ans(sd, MSG_331, user);
 
-    if(!recv_cmd(sd, "PASS", pass)){
+    if (!recv_cmd(sd, "PASS", pass))
+    {
         return 0;
     }
 
-    if(!check_credentials(user, pass)) {
+    if (!check_credentials(user, pass))
+    {
         send_ans(sd, MSG_530);
         return 0;
     }
     return send_ans(sd, MSG_230, user);
 }
 
-void operate(int sd) {
+void operate(int sd)
+{
     char op[CMDSIZE], param[PARSIZE];
     int transfer_chnl = -1;
 
-    for (;;) {
+    for (;;)
+    {
         op[0] = param[0] = '\0';
 
-        if(!recv_cmd(sd, op, param))
+        if (!recv_cmd(sd, op, param))
             return;
 
-        if (!strcmp(op, "RETR")) {
+        if (!strcmp(op, "RETR"))
+        {
             retr(transfer_chnl, param);
-        } else if (strcmp(op, "QUIT") == 0) {
+        }
+        else if (strcmp(op, "QUIT") == 0)
+        {
             send_ans(sd, MSG_221);
             close(sd);
             exit(EXIT_SUCCESS);
-        } else if (strcmp(op, "EPRT") == 0){
-            transfer_chnl = eprt(sd, param);
-        } else if(strcmp(op, "SYST") == 0){
+        }
+        else if (strcmp(op, "EPSV") == 0)
+        {
+            transfer_chnl = epsv(sd);
+        }
+        else if (strcmp(op, "SYST") == 0)
+        {
             send_ans(sd, MSG_215);
-        } else if(strcmp(op, "FEAT") == 0) {
+        }
+        else if (strcmp(op, "FEAT") == 0)
+        {
             send_ans(sd, MSG_211);
+        }
+        else
+        {
+            send_ans(sd, MSG_502);
         }
     }
 }
 
-// TODO improve error checking
-int eprt(int cmd_chnl, char* cli_addr) {
-    char *addr_family = strtok(cli_addr, EPRT_DELIM);
-    char *ip_addr = strtok(NULL, EPRT_DELIM);
-    char *port = strtok(NULL, EPRT_DELIM);
-    int cli_sd = -1;
-    struct addrinfo hints, *servinfo, *p;
+int epsv(int cmd_chnl)
+{
+    int sd = opensock_tcpsrv("127.0.0.1", NULL);
+    if(sd == -1) {
+        send_ans(cmd_chnl, MSG_227);
+        return sd;
+    }
 
-    hints.ai_family = atoi(addr_family);
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = 0;
-    hints.ai_protocol = 0;
+    send_ans(cmd_chnl, MSG_227);
+    return -1;
+}
 
-    getaddrinfo(ip_addr, port, &hints, &servinfo);
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        cli_sd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-        if(cli_sd == -1) {
+int opensock_tcpsrv(char *address, char *service) 
+{
+    int sd, ssd, addrstat, yes = 1;
+    struct addrinfo hints;
+    struct addrinfo *p, *rp;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;     /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* TCP connection */
+    hints.ai_flags = AI_PASSIVE;     /* For wildcard IP address */
+
+    addrstat = getaddrinfo(address, service, &hints, &p);
+    if (addrstat != 0)
+    {
+        fprintf(stderr, "getaddrinfo string: %s\n", gai_strerror(addrstat));
+        return -1;
+    }
+
+    // bind the first socket found
+    for (rp = p; rp != NULL; rp = rp->ai_next){
+        sd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (sd == -1) {
             perror("socket");
             continue;
         }
 
-        if(connect(cli_sd, p->ai_addr, p->ai_addrlen) != -1){
-            break;
+        if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            perror("setsockopt");
+            return -1;
         }
 
-        perror("connect");
-        close(cli_sd);
+        if (bind(sd, rp->ai_addr, rp->ai_addrlen) == -1) {
+            perror("bind");
+            close(sd);
+        }
+
+        break; // socket binded, success
     }
 
-    send_ans(cmd_chnl, MSG_227);
-    return cli_sd;
+    if (!rp) {
+        fprintf(stderr, "Server failed to bind\n");
+        return -1;
+    }
+
+    freeaddrinfo(p);
+
+    // and return socket
+    return sd;
 }
